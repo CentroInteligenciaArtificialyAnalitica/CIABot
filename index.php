b2ddde37ec43654e85685c379f523aaf7b2b40cf
juan <jotabeles.web@gmail.com>
2017-05-11 08:28:15 -0500

    agregando fin de carrera

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 // Guardamos en un arreglo la informacion de input.
 $input = json_decode(file_get_contents('php://input'), true);
 
-$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+//$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$senderId = $input->entry[0]->messaging[0]->sender->id;
 
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $answer = "I don't understand. Ask me 'hi'.";
@@ -25,11 +26,11 @@ $response = [
 
 $data = array(
 	'recipient' => array('id'=>"$senderId"),
-	'message' => array('text' => $answer)
+	'message' => array('text' => "un gusto conocerte")
 );
 
 $options = array(
-	'https' => array(
+	'http' => array(
 		'method' => 'POST',
 		'content' => json_encode($data),
 		'header' => "Content-Type: application/json\n"
