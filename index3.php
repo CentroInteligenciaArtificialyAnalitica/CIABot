d4e8a81dc24f49afb0fef94b31e059dfb66f19b3
juan <jotabeles.web@gmail.com>
2017-05-11 01:10:53 -0500

    cambios

--- index3.php
+++ index3.php
@@ -8,9 +8,11 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
   echo $_REQUEST['hub_challenge'];
   exit;
 }
-// handle bot's anwser
+// Guardamos en un arreglo la informacion de input.
 $input = json_decode(file_get_contents('php://input'), true);
+
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $answer = "I don't understand. Ask me 'hi'.";
 if($messageText == "hi") {
@@ -21,10 +23,19 @@ $response = [
     'message' => [ 'text' => $answer ]
 ];
 
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-curl_exec($ch);
-curl_close($ch);
-//based on http://stackoverflow.com/questions/36803518
+$data = array(
+	'recipient' => array('id'=>$senderId),
+	'message' => array('text' => $answer)
+);
+
+$options = array(
+	'https' => array(
+		'method' => 'POST',
+		'content' => json_encode($data),
+		'header' => 'Content-Type: application/json'
+	)
+);
+
+$context = stream_context_create($options);
+
+file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$accessToken", false, $context);
