7bad0f7e5c9dd137cee0530512853f9d86cfa5ac
juan <jotabeles.web@gmail.com>
2017-05-13 16:21:06 -0500

    comentarios de reunion

--- index.php
+++ index.php
@@ -8,27 +8,38 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
   echo $_REQUEST['hub_challenge'];
   exit;
 }
+
+
 // handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+
+// aca colocamos nuestro algoritmo
+
 $answer = "I don't understand. Ask me 'hi'.";
 if($messageText == "hi") {
     $answer = "Hello, welcome to the future ... ";
 }
+
+// termina nuestro algoritmo
+
 $response = [
     'recipient' => [ 'id' => $senderId ],
     'message' => [ 'text' => $answer ]
 ];
 
+
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $result = curl_exec($ch);
+    curl_close($ch);
 }
 //curl_exec($ch);
-curl_close($ch);
+
 
 //based on http://stackoverflow.com/questions/36803518
