1afd8a33706767851e89b3116f823ca4ff6f0e88
juan <jotabeles.web@gmail.com>
2017-06-17 15:38:36 -0500

    prueba 1

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("../require/funciones.basicas.php");
+//require_once("../require/funciones.basicas.php");
 $hubVerifyToken = 'TOKEN123456abcd';
 
 if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
